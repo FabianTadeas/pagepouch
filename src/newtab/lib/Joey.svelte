42818ae8c405646b9d868d74800310e346d898c5
FabianTadeas <services@tadafaba.eu>
2023-08-20 20:14:25 +0200

    confirm to delete

--- src/newtab/lib/Joey.svelte
+++ src/newtab/lib/Joey.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
     import IDB from "../../IDB"
     import { IconTrash } from "@tabler/icons-svelte"
+    import { twMerge } from "tailwind-merge"
 
     export let title: string;
     export let url: string;
     export let icon: string|undefined=undefined;
     export let added: number;
     export let id: number|undefined=undefined
+
+    let deletionState = 0
+    $: if (deletionState === 2){
+        IDB.joeys.delete(id)
+    }
+    let timeout: NodeJS.Timeout;
+    $: if (deletionState === 1) {
+        timeout = setTimeout(() => (deletionState = 0), 2500);
+    } else {
+        clearTimeout(timeout);
+    }
 </script>
 
 <a on:keydown={(ev)=>{
@@ -25,11 +37,17 @@
           {title}
         </p>
     </div>
-    <div class="aspect-square h-full grid place-items-center">
-        <button class="grid place-items-center aspect-square h-3/4 hover:bg-slate-400 dark:hover:bg-slate-600 transition-colors rounded-md" on:click|preventDefault={()=>{
-IDB.joeys.delete(id)
+    <div class="aspect-square h-full grid place-items-center" >
+        <button class={twMerge(
+            "grid place-items-center aspect-square h-3/4 transition-colors rounded-md",
+            deletionState === 0 ? "bg-slate-400 dark:bg-slate-600 dark:bg-opacity-0 bg-opacity-0"
+            : "bg-red-600 dark:bg-red-400 dark:bg-opacity-75 bg-opacity-75",
+            "hover:bg-opacity-100 dark:hover:bg-opacity-100"
+        )}
+        on:click|preventDefault={()=>{
+            deletionState++
         }}>
-            <IconTrash class="text-red-600 dark:text-red-400"/>
+            <IconTrash class={deletionState===1?"text-slate-300 dark:text-slate-700":"text-red-600 dark:text-red-400"}/>
         </button>
     </div>
 </a>
