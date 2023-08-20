e3296dcdb2897450c251f869bdb541643c960d63
FabianTadeas <services@tadafaba.eu>
2023-08-20 21:09:58 +0200

    buy me a coffee button

--- src/newtab/App.svelte
+++ src/newtab/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import Table from "./lib/Table.svelte";
+    import BuyMeACoffee from "./lib/BuyMeACoffee.svelte";
 </script>
 <main class="w-screen h-screen p-8 bg-slate-100 dark:bg-slate-900 grid place-items-center">
     <Table></Table>
 </main>
+<BuyMeACoffee />
