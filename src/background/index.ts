daaad15bb62d7436807cac9cd1fc8dbc5f2e7f17
FabianTadeas <services@tadafaba.eu>
2023-08-20 16:57:46 +0200

    add to pouch button color behavior

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,9 +1,9 @@
 import IDB from "../IDB";
 const target = import.meta.env.VITE_TARGET
 
-function callback(tab: browser.tabs.Tab | chrome.tabs.Tab) {
+function addJoey(tab: browser.tabs.Tab | chrome.tabs.Tab) {
   IDB.joeys.add({
-      title: tab.title,
+    title: tab.title,
       added: new Date().getTime(),
       url: tab.url,
       icon: tab.favIconUrl
@@ -11,7 +11,13 @@ function callback(tab: browser.tabs.Tab | chrome.tabs.Tab) {
 }
 
 if (target === "v2") {
-  browser.browserAction.onClicked.addListener(callback)
+  browser.browserAction.onClicked.addListener((tab)=>{
+    browser.browserAction.setIcon({path: "/favicon_green.svg", tabId:tab.id})
+    addJoey(tab)
+  })
 } else {
-  chrome.action.onClicked.addListener(callback)
+  chrome.action.onClicked.addListener((tab)=>{
+    chrome.action.setIcon({path: "/favicon_green.svg", tabId:tab.id});
+    addJoey(tab)
+  })
 }
