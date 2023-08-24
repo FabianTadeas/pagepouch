f1cdfeb993eff56b7985ffd5ed3473acb049609c
Tadeas Fabian <services@tadafaba.eu>
2023-08-24 15:06:02 +0200

    directory support

--- src/IDB.ts
+++ src/IDB.ts
@@ -1,12 +1,12 @@
 import Dexie, { type Table } from "dexie";
 
 class IDB extends Dexie {
-  joeys!: Table<{id?:number,title:string,url:string,icon?:string,added:number}>;
+  joeys!: Table<{id?:number,title:string,url:string,icon?:string,added:number,parentId:number}>;
 
   constructor() {
     super("pagepouchIDB");
     this.version(1).stores({
-      joeys: "++id, title, added, url",
+      joeys: "++id, title, added, url, parentId",
     });
   }
 }
