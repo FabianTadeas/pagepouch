23f1fd2b3c1914972e9da73025e8181344cf4c40
Tadeas Fabian <services@tadafaba.eu>
2023-09-04 19:17:49 +0200

    changes to navbar

--- src/newtab/lib/Nav.svelte
+++ src/newtab/lib/Nav.svelte
@@ -17,22 +17,25 @@
   class="dark:text-white text-base font-sans-bold flex justify-between px-16"
 >
   <a
-    class="p-2 underline-offset-2 hover:underline flex gap-2"
+    class="p-2 underline-offset-2 hover:underline flex gap-2 active-outline rounded-md"
     href="https://github.com/FabianTadeas/pagepouch"
   >
     <img src="/favicon.svg" alt="" />
     {chrome.i18n.getMessage("extName")}
   </a>
   <div class="flex gap-6">
-    <button class="p-2 transition-all hover:text-accent">
-      <IconSearch />
-    </button>
+    <a
+      class="p-2 transition-colors hover:text-accent active-outline rounded-md"
+      href="https://www.buymeacoffee.com/tadafaba"
+    >
+      <IconCup />
+    </a>
     <button
-      class="p-2 transition-all hover:text-accent"
+      class="p-2 transition-colors hover:text-accent active-outline rounded-md"
       on:click={() => {
         let title = prompt("title");
         IDB.tiles.add({
-          type:"directory",
+          type: "directory",
           title,
           added: new Date().getTime(),
           parentId: $activeDir,
@@ -42,12 +45,12 @@
       <IconFolderPlus />
     </button>
     <button
-      class="p-2 transition-all hover:text-accent"
+      class="p-2 transition-colors hover:text-accent active-outline rounded-md"
       on:click={() => {
         let title = prompt("title");
         let url = prompt("url");
         IDB.tiles.add({
-          type:"bookmark",
+          type: "bookmark",
           title,
           added: new Date().getTime(),
           url,
@@ -57,28 +60,20 @@
     >
       <IconBrowserPlus />
     </button>
-    <button class="p-2 transition-all hover:text-accent">
+    <button class="p-2 transition-colors hover:text-accent active-outline rounded-md">
       <IconSettings />
     </button>
     <button
       class="p-2 transition-all hover:text-accent"
       on:click={() => {
-        location.hash = prompt("id")
+        location.hash = prompt("id");
       }}
     >
       <IconTestPipe />
     </button>
-    <button class="p-2 transition-all hover:text-accent">
-      <IconBrandGithub />
-    </button>
-    <a
-      class="p-2 transition-all hover:text-accent"
-      href="https://www.buymeacoffee.com/tadafaba"
-    >
-      <IconCup />
-    </a>
   </div>
-  <button class="p-2 underline-offset-2 hover:underline flex gap-2">
+  <!-- <div /> -->
+  <button class="p-2 underline-offset-2 hover:underline flex gap-2 active-outline rounded-md">
     Log In
     <IconUser />
   </button>
